7e94a0a6c5a5ddfb9a910a13c695327747354b82
gunhee <rjsgml771@naver.com>
2024-07-10 16:36:25 +0900

    link test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,61 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { connect } from '@/redux/slice/auth-slice';
 import { RootState } from '@/redux/store';
-import { useState } from 'react';
-import {stat} from "fs";
+import {useEffect, useState} from "react";
+import "./styles/globals.css";
 
 const Home = () => {
-    const code = useSelector((state: RootState) => state.data.code);
-    const text = useSelector((state: RootState) => state.data.text);
-    const list = useSelector((state: RootState) => state.data.list);
-
+    const data = useSelector((state: RootState) => state.data);
+    const code = data ? data.code : "";
+    const text = data ? data.text : "";
     const dispatch = useDispatch();
-    let dragge = false;
+
+    const [linktoCT, setLinktoCT] = useState("none");
+    const [drage, setDrage] = useState(false);
+    const [link, setLink] = useState([]); // code와 설명의 연결 배열
+    const [copyText, setCopyText] = useState([]);
+
+    const handleMouseUp = () => {
+        if (drage && linktoCT=="during") {
+            const selection = window.getSelection();
+            if (selection) {
+                const selectedText = selection.toString();
+                setCopyText((prevText) => [...prevText, selectedText]);
+
+            }
+            setDrage(false);
+        }
+    };
+    useEffect(() => {
+        if (linktoCT === "done") {
+            const newLink = {"code": copyText[1], "text": copyText[0]};
+            setLink((prevLink) => [...prevLink, newLink]);
+            setLinktoCT("none");
+            setCopyText([]);
+        }
+    }, [linktoCT]);
+
+    useEffect(() => {
+        console.log(copyText);
+    }, [copyText]);
+    useEffect(() => {
+        console.log(link);
+    }, [link]);
 
 
     return (
-        <div>
-            <input type="submit" value="start" onSubmit={() => {
-                dragge != dragge;
-            }} />
-            <input type="submit" value="end" onSubmit={() => {
-
-            }} />
-            <div className="flex-row-reverse">
-                <p>{code}</p>
-                <p>{text}</p>
-                <p>{dragge}</p>
+        <div className="my-2 mx-4 overflow-hidden w-fit">
+            <input className="mx-2 my-1 hover:text-sky-300 hover:font-bold" type="submit" value="start" onClick={() => setLinktoCT("during")} />
+            <input className="mx-2 my-1 hover:text-orange-300 hover:font-bold" type="submit" value="end" onClick={() => setLinktoCT("done")} />
+            <span className="mx-2 my-1 font-bold">{linktoCT.toString()}</span>
+            <div className="flex flex-row justify-center overflow-hidden w-full" onMouseUp={handleMouseUp}>
+                <pre className="max-h- overflow-y-auto whitespace-pre-wrap w-2/5
+                bg-black opacity-65 text-white px-3 py-2 rounded-lg
+                "
+                onMouseDown={() => setDrage(true)}>{code}</pre>
+                <pre className="max-h-60 overflow-y-auto whitespace-pre-wrap w-2/5
+                px-3 py-2 my-auto
+                " onMouseDown={() => setDrage(true)}>{text}</pre>
             </div>
         </div>
     );
