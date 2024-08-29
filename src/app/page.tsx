8a20fae86fee9e1a61fa2b1d4a6d48f6ceca177d
gunhee <rjsgml771@naver.com>
2024-08-29 18:19:06 +0900

    fix/pr[6]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,13 @@
-'use client';
+"use client";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { connect } from '@/redux/slice/auth-slice';
-import { RootState } from '@/redux/store';
-import { useState } from 'react';
-import {stat} from "fs";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useState } from "react";
+import { stat } from "fs";
+import HomePage from "@/components/home/HomePage";
 
 const Home = () => {
-    const code = useSelector((state: RootState) => state.data.code);
-    const text = useSelector((state: RootState) => state.data.text);
-    const list = useSelector((state: RootState) => state.data.list);
-
-    const dispatch = useDispatch();
-    let dragge = false;
-
-
-    return (
-        <div>
-            <input type="submit" value="start" onSubmit={() => {
-                dragge != dragge;
-            }} />
-            <input type="submit" value="end" onSubmit={() => {
-
-            }} />
-            <div className="flex-row-reverse">
-                <p>{code}</p>
-                <p>{text}</p>
-                <p>{dragge}</p>
-            </div>
-        </div>
-    );
+  return <HomePage />;
 };
 
 export default Home;
--- src/hooks/ReduxProvider.tsx
+++ src/hooks/ReduxProvider.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import React from "react";
-import { persistor, store } from "../redux/store";
+import { store } from "../redux/store";
 import { Provider } from "react-redux";
 
 // redux를 제공하는 provider를 사용해 ssr -> csr로 출력하기 위함
 export default function ReduxProvider({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return <Provider store={store}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 }
